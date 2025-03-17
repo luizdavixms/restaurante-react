30a5a26e240b0d9fbc0b37429ce0b628ff1eb2cf
kauan oliveira <kauanoliveira0108@gmail.com>
2025-03-17 17:09:37 -0300

    apenas css do kauan

--- site-restaurante/src/pages/Home/index.jsx
+++ site-restaurante/src/pages/Home/index.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
 
-function Home() {
-  return(
-    <div>
-      <h1>Home</h1>
+
+function App() {
+  return (
+    <div style={{ 
+       backgroundImage: "linear-gradient(109.6deg,#B22222  11.2%, BLACK 91.1%)",  
+      backgroundColor: "#1D1D1D", 
+      minHeight: "100vh", 
+      display: "flex", 
+      flexDirection: "column", 
+      justifyContent: "center", 
+      alignItems: "center", 
+      color: "white",
+      textAlign: "center"
+    }}>
+
+      <h1>Bem-vindo ao Restaurante!</h1>
     </div>
   );
 }
 
-export default Home;
+export default App;
