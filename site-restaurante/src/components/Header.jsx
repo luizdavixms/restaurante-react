857684ef757e5eb11fab62a9dedd1762a47ecdaf
Mackenzie <mackenzie@MacBook-Pro-de-Mackenzie.local>
2025-03-18 08:47:28 -0300

    adicionando a logo e css

--- site-restaurante/src/components/Header.jsx
+++ site-restaurante/src/components/Header.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
+import "./Header.css"
+import Logo from "../assets/Logo.png"
 import { Link } from "react-router-dom"
 
 function Header() {
   return (
     <header>
-        <div className='logo'>
-            <img src='' alt='Logo do Restaurante' className='logoImagem'/>
-        </div>
-
+      <Link to="/">
+        <img src={Logo} alt='Logo do Restaurante' className='logoImagem'/>
+      </Link>  
         <nav className='containerNavLinks'>
             <Link to="/Cardapio" className='navLink'>Cardapio</Link>
             <Link to="/Sobre" className='navLink'>Sobre</Link>
             <Link to="/Localizacao" className='navLink'>Localizacao</Link>
             <Link to="/Reservas" className='navLink'>Reservas</Link>
         </nav>
+        <div className='linhaSeparacao'>
+        
+        </div>
     </header>
   );
 }
