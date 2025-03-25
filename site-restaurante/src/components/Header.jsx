156c31d52f770783779fd90720a8d291f775ba1a
Luiz Davi Ximenes <luizdavi.xms04@gmail.com>
2025-03-25 15:30:16 -0300

    Refazendo o Header

--- site-restaurante/src/components/Header.jsx
+++ site-restaurante/src/components/Header.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
 import "./Header.css"
-import Logo from "../assets/Logo.png"
+import logoSimples from "../assets/logoSimples.png"
 import { Link } from "react-router-dom"
 
 function Header() {
   return (
     <header>
       <Link to="/">
-        <img src={Logo} alt='Logo do Restaurante' className='logoImagem'/>
+        <img src={logoSimples} alt='Logo do Restaurante' className='logoImagem'/>
       </Link>  
         <nav className='containerNavLinks'>
-            <Link to="/Cardapio" className='navLink'>Cardapio</Link>
-            <Link to="/Sobre" className='navLink'>Sobre</Link>
-            <Link to="/Localizacao" className='navLink'>Localizacao</Link>
-            <Link to="/Reservas" className='navLink'>Reservas</Link>
+            <Link to="/Cardapio" className='navLink'>CARDÁPIO</Link>
+            <Link to="/Sobre" className='navLink'>SOBRE</Link>
+            <Link to="/Localizacao" className='navLink'>LOCALIZAÇÃO</Link>
+            <Link to="/Reservas" className='navLink'>RESERVAS</Link>
         </nav>
-        <div className='linhaSeparacao'></div>
-        <div></div>
     </header>
   );
 }
